d695c221d57e43c356cdedab090273947e6b0c8d
JustDiablot <valent1.auffray@gmail.com>
2023-11-09 16:34:00 +0100

    added comment

--- functions.py
+++ functions.py
@@ -3,8 +3,8 @@ import os
 
 def files_list(directory, ext):
     files_name = []
-    for filename in os.listdir(directory):
-        if filename.endswith(ext):
-            files_name.append(filename)
-    return files_name
+    for filename in os.listdir(directory):  #for each files in the folder
+        if filename.endswith(ext):          #only if it end by '.txt'
+            files_name.append(filename)     #add the name of the file into the list files_name
+    return files_name                       #return the list
 
