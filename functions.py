ad03db3d12f037ae54fbebd3eb063982cf35d5a2
Telurion <telurion.ytb@gmail.com>
2023-11-16 14:09:25 +0100

    Suppression de l'ajout de l'apostrophe

--- functions.py
+++ functions.py
@@ -13,10 +13,7 @@ def get_names(files_name):                                                      
     extract_name = []
     deleted_characters = ["Nomination_", "1", "2", '.txt']                                  # List with each character that we want to delete
     for _ in range(len(files_name)):                                                        # For each position in the list of the names of the files
-        if files_name[_] == 'Nomination_Giscard dEstaing.txt':                              # If the string is Nomination_Giscard dEstaing.txt
-            extract_name.append('Nomination_Giscard d\'Estaing.txt')                        # Put an apostrophe in the name
-        else:                                                                               # If the string is not Nomination_Giscard dEstaing.txt
-            extract_name.append(files_name[_])                                              # Add to a new list each string
+        extract_name.append(files_name[_])                                                  # Add to a new list each string
     for i in range(len(extract_name)):                                                      # For each string in the new list
         for j in range(len(deleted_characters)):                                            # For each unwanted character
             extract_name[i] = extract_name[i].replace(deleted_characters[j], "")            # Delete the unwanted character in each string
@@ -26,7 +23,7 @@ def get_names(files_name):                                                      
 def add_names(extract_name):                                                                ## Combine the first name and the last name
     president_name = []
     first_names = {'Chirac' : 'Jacques',
-                   'Giscard d\'Estaing' : 'Valéry',
+                   'Giscard dEstaing' : 'Valéry',
                    'Hollande' : 'François',
                    'Macron' : 'Emmanuel',
                    'Mitterrand' : 'François',
