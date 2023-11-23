ead4f87db845c095c8e7cd82ebc7eda660a19978
JustDiablot <valent1.auffray@gmail.com>
2023-11-23 14:24:17 +0100

    encoding

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ files_name = functions.files_list(directory, '.txt')
 extract_name = functions.get_names(files_name)
 president_name = functions.add_names(extract_name)
 list_name = functions.delete_doubles(president_name)
-print(list_name)
 
 functions.create_folder()
 functions.create_file(files_name)
 functions.copy_text(files_name)
 
 ss = tfidf.listing(files_name, 1)
-tf = tfidf.tf(files_name, 1, ss)
-idf = tfidf.idf(tf)
+#tf = tfidf.tf(files_name, 1, ss)
+#idf = tfidf.idf(tf)
+
+print(ss)
--- tfidf.py
+++ tfidf.py
@@ -4,7 +4,7 @@ import math
 def listing(files_name, i):
     l = []
     l1 = []
-    f = open(f'./speeches-20231109/{files_name[i]}', 'r')
+    f = open(f'./speeches-20231109/{files_name[i]}', 'r',encoding='utf8')
     lines = f.readlines()
     for line in lines:  
         l = line.split()
@@ -12,15 +12,17 @@ def listing(files_name, i):
             l1.append(i)
     return set(l1)
 
-def tf(files_name, i,  listing):
+"""def tf(files_name, i,  listing):
     tf = {}
-    with open(f'./speeches-20231109/{files_name[i]}', 'r') as f:
+    with open(f'./speeches-20231109/{files_name[i]}', 'r',encoding='utf8') as f:
         for word in listing:
             tf[word] = 0
-        for word in listing:
-            [word] += 1
+        for lines in f.readlines():
+            for line in lines:
+                for word in line:
+                    tf[word] += 1
         return tf
-
+"""
 def idf(tf):
     idf = dict(tf)
     for key in idf.keys():
