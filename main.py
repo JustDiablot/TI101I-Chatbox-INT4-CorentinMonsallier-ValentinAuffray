eff0824de49402b9a2d46a88192dcf390db4caec
Telurion <telurion.ytb@gmail.com>
2023-11-25 19:52:44 +0100

    Rectification des fonctions tf et idf + commentaires

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import functions
 import tfidf
 
-directory = './speeches-20231109'
-files_name = functions.files_list(directory, '.txt')
+speeches = './speeches-20231109'
+cleaned = './cleaned'
+files_name = functions.files_list(speeches, '.txt')
 extract_name = functions.get_names(files_name)
 president_name = functions.add_names(extract_name)
 list_name = functions.delete_doubles(president_name)
@@ -11,10 +12,4 @@ functions.create_folder()
 functions.create_file(files_name)
 functions.copy_text(files_name)
 
-ss = tfidf.listing(files_name, 1)
-tf = tfidf.tf(files_name, 1, ss)
-idf = tfidf.idf(tf)
-tf_idf = tfidf.tf_idf(tf, idf)
-print(tf, "\n\n")
-print(idf, "\n\n")
-print(tf_idf)
+print(tfidf.idf(cleaned))
--- tfidf.py
+++ tfidf.py
@@ -1,36 +1,34 @@
 import math
+import os
 
-
-def listing(files_name, i):
-    l = []
-    l1 = []
-    f = open(f'./cleaned/{files_name[i]}', 'r',encoding='utf8')
-    lines = f.readlines()
-    for line in lines:  
-        l = line.split()
-        for i in l:
-            l1.append(i)
-    return l1
-
-def tf(files_name, i,  listing):
+def tf(file_path):                                                                                                      ## Look for the frequency of each word in a text
+    file = open(file_path, 'r', encoding='utf-8')                                                                       # Opens a speech
     tf = {}
-    print(files_name[i])
-    with open(f'./cleaned/{files_name[i]}', 'r',encoding='utf8') as f:
-    
-        for word in set(listing):
-            tf[word] = 0
-        for word in listing:
-            tf[word] += 1
-        return tf
+    list = []
+    lines = file.readlines()                                                                                            # Read all lines in a file
+    for line in lines:                                                                                                  # For each line of this file
+        for i in line.split():                                                                                          # For each word of each line
+            list.append(i)                                                                                              # Add it to a list
+    for word in list:                                                                                                   # For each word of the list
+        if word not in tf:                                                                                              # If the word is not in the tf dictionnary
+            tf[word] = 1                                                                                                # Add it with a value of 1
+        else:                                                                                                           # If the word is already in the dictionnary
+            tf[word] += 1                                                                                               # Add to the value of the count 1
+    return tf                                                                                                           # Return the tf dictionary
 
-def idf(tf):
-    idf = dict(tf)
-    for key in idf.keys():
-        idf[key] = 1 / math.log(idf[key], 10)
-    return idf
 
-def tf_idf(tf, idf):
-    tf_idf = dict(tf)
-    for key in tf_idf.keys():
-        tf_idf[key] = tf[key] * idf[key]
-    return tf_idf
+def idf(file_path):                                                                                                      ## Get the importance of each term
+    idf = {}
+    for file in os.listdir(file_path):
+        for key in tf(f'{file_path}/{file}').keys():
+            if key not in idf:
+                idf[key] = 1
+            else:
+                idf[key] += 1                                                                                           # Add to the value of the the count
+    for key in idf.keys():                                                                                              # For each word of the list
+        idf[key] = math.log(8 / idf[key])                                                                               # Compute the logarithm of the inverse of the proportion of documents in the corpus that contain that word
+    return(idf)                                                                                                         # Return the tdf dictionnary
+
+
+def tf_idf(directory):
+    pass
